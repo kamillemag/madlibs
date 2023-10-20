e7dc20c5996b8a826f00953ecb5203ff9838c5b2
Kamille Magdirila <kamillemagdirila@Kamilles-Air.lan>
2023-10-20 15:20:39 -0700

    basic version

--- app.py
+++ app.py
@@ -1,45 +1,20 @@
 """Madlibs Stories."""
+from flask import Flask, render_template, request
+# from flask_debugtoolbar import DebugToolbarExtension
+from stories import story
 
+app = Flask(__name__)
+# app.config['SECRET_KEY'] = 'pugzy'
+# debug = DebugToolbarExtension(app)
 
-class Story:
-    """Madlibs story.
+@app.route('/')
+def home_page():
+    prompts = story.prompts
+    return render_template('index.html', prompts=prompts)
 
-    To  make a story, pass a list of prompts, and the text
-    of the template.
+@app.route('/story')
+def story_result():
+    """ displays user's story"""
 
-        >>> s = Story(["noun", "verb"],
-        ...     "I love to {verb} a good {noun}.")
-
-    To generate text from a story, pass in a dictionary-like thing
-    of {prompt: answer, promp:answer):
-
-        >>> ans = {"verb": "eat", "noun": "mango"}
-        >>> s.generate(ans)
-        'I love to eat a good mango.'
-    """
-
-    def __init__(self, words, text):
-        """Create story with words and template text."""
-
-        self.prompts = words
-        self.template = text
-
-    def generate(self, answers):
-        """Substitute answers into text."""
-
-        text = self.template
-
-        for (key, val) in answers.items():
-            text = text.replace("{" + key + "}", val)
-
-        return text
-
-
-# Here's a story to get you started
-
-
-story = Story(
-    ["place", "noun", "verb", "adjective", "plural_noun"],
-    """Once upon a time in a long-ago {place}, there lived a
-       large {adjective} {noun}. It loved to {verb} {plural_noun}."""
-)
+    text = story.generate(request.args)
+    return render_template("story.html", text=text)
